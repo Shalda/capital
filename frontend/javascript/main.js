6c245921649e0c1ef0f8e897c44f7972b62bbd7f
Shalda <zeppelin.ua@gmail.com>
2016-11-22 09:57:36 +0200

    1

---
+++ frontend/javascript/main.js
@@ -0,0 +1,61 @@
+requirejs.config({
+    baseUrl: './js'
+});
+
+define(["jquery", "domReady", "jquery.maskedinput"], function ($) {
+    "use strict";
+    //burger menu header
+    $('.header-navigation__item').hover(function () {
+        $(this)
+            .find('#submenu')
+            .stop()
+            .slideToggle()
+    });
+    
+    //arrow-icon for language 
+    $('.phone-button').click(function () {
+        $(this)
+            .stop()
+            .toggleClass('active_arrow'),
+            $('.header-phone')
+                .find('.phone-menu')
+                .stop()
+                .slideToggle('fast')
+    });
+
+    //arrow-icon for language 
+    $('.lang-button').click(function () {
+        $(this)
+            .stop()
+            .toggleClass('active_arrow'),
+            $('#language')
+                .find('.language-menu')
+                .stop()
+                .slideToggle('fast')
+    });
+
+    //phonenumber input
+    $("#phone-field").mask("+38(999)99-99-999");
+
+
+    //search toogle
+    $('.search').click(function () {
+        $('.search-form ')
+            .stop()
+            .toggleClass('active_field'),
+            $(' #search-field')
+                .stop()
+                .toggleClass('active_search')
+
+    });
+    $('.icon-cross').click(function () {
+        $('.search-form ')
+            .stop()
+            .toggleClass('active_field'),
+            $(' #search-field')
+                .stop()
+                .toggleClass('active_search')
+
+    });
+
+});
